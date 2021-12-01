2bb2f0b03ef4e0a6d1bc89614894f269eece2335
Yujia Yan <yujiay2@illinois.edu>
2021-12-01 00:58:59 -0600

    fix flaky

--- src/test/java/com/amazon/dtasdk/v2/serialization/messages/InstantAccessRequestTest.java
+++ src/test/java/com/amazon/dtasdk/v2/serialization/messages/InstantAccessRequestTest.java
@@ -36,19 +36,19 @@ public class InstantAccessRequestTest {
 
         String requestString = serializer.encode(request);
         requestString = requestString.replace("{","").replace("}","").replace("\"","");
-        HashMap<String, String> payloadMap = new HashMap<String, String>();
+        HashMap<String, String> requestMap = new HashMap<String, String>();
         String[] pairs = requestString.split(",");
         for (int i=0; i<pairs.length; i++) {
                 String pair = pairs[i];
                 String[] keyValue = pair.split(":");
-                payloadMap.put(keyValue[0], keyValue[1]);
+                requestMap.put(keyValue[0], keyValue[1]);
         }
         HashMap<String, String> expected = new HashMap<String, String>();
         expected.put("operation", "GetUserId");
         expected.put("infoField1", "nobody@amazon.com");
         expected.put("infoField2", "AMZN");
 
-        assertEquals(payloadMap, expected);
+        assertEquals(requestMap, expected);
     }
 
     @Test
